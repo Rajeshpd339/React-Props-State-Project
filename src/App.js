ad250bb44bb547cfe191a33e7e21b9878c203dd8
Rajeshpd339 <rajeshpd339@gamil.com>
2023-10-20 19:23:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar title="Textutils2" aboutText="About Us" /> {/* here we are passing props as a title */}
       <div className='container'>
         <TextForm  heading="Enter the text Below.."/> {/*passing heading as a props  */}
+        <About/>
       </div>
     </>
   );
